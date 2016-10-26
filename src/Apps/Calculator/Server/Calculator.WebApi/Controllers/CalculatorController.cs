e2c108707cc04d17066134a1708bfd197632c8cd
roi@saturn72.com <roi@saturn72.com>
2016-10-26 18:24:36 +0300

    Add web api to the calculator app

---
+++ src/Apps/Calculator/Server/Calculator.WebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿#region
+
+using System.Net.Http;
+using System.Web.Http;
+using Saturn72.Common.WebApi;
+
+#endregion
+
+namespace Calculator.WebApi.Controllers
+{
+    [RoutePrefix("api/calc")]
+    public class CalculatorController : Saturn72ApiControllerBase
+    {
+        [Route("add/{x}/{y}")]
+        [HttpGet]
+        public IHttpActionResult Add(HttpRequestMessage request, int x, int y)
+        {
+            return Ok(x + y);
+        }
+    }
+}
---
+++ src/Apps/Calculator/Server/Tests/Calculator.WebApi.Tests/Controllers/CalculatorControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http.Results;
+using NUnit.Framework;
+using  Calculator.WebApi.Controllers;
+using Saturn72.UnitTesting.Framework;
+
+namespace Calculator.WebApi.Tests.Controllers
+{
+    public class CalculatorControllerTests
+    {
+        [Test]
+        public void CalculatorController_AddsTwoNumbers()
+        {
+            var ctrl = new CalculatorController();
+            //2 positives
+            (ctrl.Add(null, 2, 3) as OkNegotiatedContentResult<int>).Content.ShouldEqual(5);
+
+            //positive + negative
+            (ctrl.Add(null, -1, 2) as OkNegotiatedContentResult<int>).Content.ShouldEqual(1);
+
+            //2 negatives
+            (ctrl.Add(null, -2, -3) as OkNegotiatedContentResult<int>).Content.ShouldEqual(-5);
+        }
+    }
+}
