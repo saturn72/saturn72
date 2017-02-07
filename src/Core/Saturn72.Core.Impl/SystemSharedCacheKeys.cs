e0573b84d71703af51fe5d5c3c9ea1aab8d6bf08
Roi Shabtai <roi@saturn72.com>
2017-02-07 12:58:52 +0200

    Fix cache key typo

--- src/Core/Saturn72.Core.Impl/SystemSharedCacheKeys.cs
+++ src/Core/Saturn72.Core.Impl/SystemSharedCacheKeys.cs
@@ -2,7 +2,7 @@
 {
     public sealed class SystemSharedCacheKeys
     {
-        public const string UserCacheKey = "saturn72.User-{0}";
+        public const string UserCacheKey = "saturn72.user-{0}";
         internal const string UserRolesUserCacheKey = "saturn72.user-{0}.userroles";
         internal const string AllUserCacheKey = "saturn72.all-users";
     }
