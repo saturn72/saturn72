b86e1e1a95f4e83e95674a950e0782390886637f
roi shabtai <roi@saturn72.com>
2017-03-05 12:58:29 +0200

    Fix hello world application

---
+++ src/Apps/HelloWorld/HelloWorld/DummyConfigManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Saturn72.Core.Configuration;
+using Saturn72.Core.Configuration.Maps;
+using Saturn72.Core.Infrastructure.AppDomainManagement;
+
+namespace HelloWorld
+{
+    public class DummyConfigManager:IConfigManager
+    {
+        public IDictionary<string, Lazy<IConfigMap>> ConfigMaps { get; }
+        public AppDomainLoadData AppDomainLoadData { get; }
+    }
+}
