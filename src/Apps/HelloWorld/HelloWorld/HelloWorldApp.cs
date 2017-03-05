c12c5d82a8381b0a089d10f88090fdcb8e1c0a43
roi shabtai <roi@saturn72.com>
2017-03-05 12:58:29 +0200

    Fix hello world application

--- src/Apps/HelloWorld/HelloWorld/HelloWorldApp.cs
+++ src/Apps/HelloWorld/HelloWorld/HelloWorldApp.cs
@@ -1,38 +1,43 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using Saturn72.Common.App;
-    using Saturn72.Core.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using Saturn72.Common.App;
+using Saturn72.Core.Configuration;
 
-    namespace HelloWorld
+namespace HelloWorld
+{
+    public class HelloWorldApp : Saturn72AppBase
     {
-        public class HelloWorldApp : Saturn72AppBase
+        private readonly IAppVersion _version = new AppVersion();
+
+        public HelloWorldApp(string appId) : base(appId)
         {
-            private readonly IAppVersion _version = new AppVersion();
-            public HelloWorldApp(string appId) : base(appId)
-            {
-            }
-
-            public HelloWorldApp(string appId, IConfigManager configManager) : base(appId, configManager)
-            {
-            }
-
-            public override void Start()
-            {
-                Console.WriteLine("Hello from " + AppId + "application");
-                Console.WriteLine("Nice to meet you");
-                base.Start();
-            }
-
-            public override string Name => "HelloWorldApp";
-            public override IEnumerable<IAppVersion> Versions => new [] {_version};
         }
 
-        public class AppVersion:IAppVersion
+        public HelloWorldApp(string appId, IConfigManager configManager) : base(appId, configManager)
         {
-            public string Key => "version_key";
-            public int Index => 0;
-            public bool IsLatest => true;
-            public bool Publish => true;
-            public AppVersionStatusType Status=> AppVersionStatusType.ReleaseCandidate;
+        }
+
+        public override string Name => "HelloWorldApp";
+        public override IEnumerable<IAppVersion> Versions => new[] {_version};
+
+        public override void Start()
+        {
+            var tmp = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello from " + AppId + "application");
+            Console.WriteLine("Nice to meet you");
+            Console.ForegroundColor = tmp;
+
+            base.Start();
         }
     }
+
+    public class AppVersion : IAppVersion
+    {
+        public string Key => "version_key";
+        public int Index => 0;
+        public bool IsLatest => true;
+        public bool Publish => true;
+        public AppVersionStatusType Status => AppVersionStatusType.ReleaseCandidate;
+    }
+}
--- src/Apps/HelloWorld/HelloWorld/Program.cs
+++ src/Apps/HelloWorld/HelloWorld/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace HelloWorld
+﻿using System.Diagnostics;
+
+namespace HelloWorld
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var consoleTraceListener = new ConsoleTraceListener();
+            Trace.Listeners.Add(consoleTraceListener);
             var app = new HelloWorldApp("hello_world");
             app.Start();
+            Trace.Listeners.Remove(consoleTraceListener);
         }
     }
 }
