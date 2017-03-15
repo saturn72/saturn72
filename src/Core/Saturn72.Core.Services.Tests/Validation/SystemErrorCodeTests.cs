96b1232e3d59bfc61a950b542995e62166efdea9
roi <roi@saturn72.com>
2017-03-15 07:53:38 +0200

    Add method t flatten SystemErrorCode

---
+++ src/Core/Saturn72.Core.Services.Tests/Validation/SystemErrorCodeTests.cs
@@ -0,0 +1,23 @@
+﻿
+using NUnit.Framework;
+using Saturn72.Core.Services.Validation;
+using Saturn72.UnitTesting.Framework;
+
+namespace Saturn72.Core.Services.Tests.Validation
+{
+    public class SystemErrorCodeTests
+    {
+     [Test]
+        public void SystemErrorCodeExtensions_Flatten_ValidateFormat()
+        {
+            var code = "code";
+            var message = "message";
+            var category = "category";
+            var subcategory = "subcategory";
+            var expected = string.Format("<= Code: {0} <=> Message: {1} <=> Category: {2} <=> Subcategory: {3} =>", code,
+                message, category, subcategory);
+
+                new SystemErrorCode(code, message, category, subcategory).Flatten().ShouldEqual(expected);
+        }
+    }
+}
---
+++ src/Core/Saturn72.Core.Services/Validation/SystemErrorCodeExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Saturn72.Core.Services.Validation
+{
+    public static  class SystemErrorCodeExtensions
+    {
+        private const string FlattenFormat = "<= Code: {0} <=> Message: {1} <=> Category: {2} <=> Subcategory: {3} =>";
+        public static string Flatten(this SystemErrorCode errorCode)
+        {
+
+            return string.Format(FlattenFormat, errorCode.Code, errorCode.Message, errorCode.Category,
+                errorCode.SubCategory);
+        }
+    }
+}
