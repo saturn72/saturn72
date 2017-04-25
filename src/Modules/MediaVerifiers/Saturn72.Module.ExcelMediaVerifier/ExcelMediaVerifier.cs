1202ae17e726aadce560ec6c137769073298e71d
roi shabtai <roi@saturn72.com>
2017-04-25 22:39:47 +0300

    ref #15
    Create partial function module for excel validation

---
+++ src/Modules/MediaVerifiers/Saturn72.Module.ExcelMediaVerifier/ExcelMediaVerifier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Saturn72.Core.Services.Impl.Media;
+using Saturn72.Core.Services.Media;
+
+namespace Saturn72.Module.ExcelMediaVerifier
+{
+    public class ExcelMediaVerifier:IMediaValidator
+    {
+        public IEnumerable<string> SupportedExtensions { get; } = new []{"xls", "xlsx"};
+
+        public MediaStatusCode Validate(byte[] bytes, string extension)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/MediaVerifiers/Saturn72.Module.ExcelMediaVerifier/ExcelMediaVerifierModule.cs
@@ -0,0 +1,19 @@
+﻿using Saturn72.Core.Extensibility;
+
+namespace Saturn72.Module.ExcelMediaVerifier
+{
+    public class ExcelMediaVerifierModule : IModule
+    {
+        public void Load()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
