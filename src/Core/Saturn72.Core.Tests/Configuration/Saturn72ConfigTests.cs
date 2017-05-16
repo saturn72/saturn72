e85905263f5465956ab91f6d87a2ac6ad88a8b31
roi shabtai <roi@saturn72.com>
2017-05-16 14:00:40 +0300

    resolves #19

--- src/Core/Saturn72.Core.Tests/Configuration/Saturn72ConfigTests.cs
+++ src/Core/Saturn72.Core.Tests/Configuration/Saturn72ConfigTests.cs
@@ -13,6 +13,7 @@ namespace Saturn72.Core.Tests.Configuration
     public class Saturn72ConfigTests
     {
         [Test]
+        [Category("non_deterministic")]
         public void GetConfiguration_GetDefaultConfig()
         {
             var tmp = AppDomain.CurrentDomain.GetData("APP_CONFIG_FILE").ToString();
