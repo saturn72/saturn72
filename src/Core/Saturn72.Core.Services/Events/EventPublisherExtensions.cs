633f830632ef33df7a32793959c06efb7a369cd5
roi <roi@saturn72.com>
2017-08-12 21:37:54 +0300

    Remove JSON lib from Saturn72.Core.Services

--- src/Core/Saturn72.Core.Services/Events/EventPublisherExtensions.cs
+++ src/Core/Saturn72.Core.Services/Events/EventPublisherExtensions.cs
@@ -1,7 +1,7 @@
 ﻿#region
 
 using System;
-using Newtonsoft.Json;
+using fastJSON;
 using Saturn72.Core.Domain;
 using Saturn72.Extensions;
 
@@ -40,25 +40,6 @@ namespace Saturn72.Core.Services.Events
         {
             var eventMessage = new DeletedEvent<TDomainModel>(domainModel);
             eventPublisher.Publish(eventMessage);
-        }
-
-
-        /// <summary>
-        ///     Publishes event from json
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="typeFullName">
-        ///     Type full name. Use the following format: {type_full_name}, {assembly_name}.
-        ///     <example>System.string, mscorlib</example>
-        /// </param>
-        public static void Publish(string value, string typeFullName)
-        {
-            var type = Type.GetType(typeFullName);
-            var eventBaseType = typeof(EventBase);
-            Guard.MustFollow(eventBaseType.IsAssignableFrom(type),
-                "The sent type does not derive from " + eventBaseType.Name);
-
-            var o = JsonConvert.DeserializeObject(value, type);
         }
     }
 }
