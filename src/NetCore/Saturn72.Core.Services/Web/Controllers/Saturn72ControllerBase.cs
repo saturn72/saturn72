8449fe203446ef84f5ddf139b5ef621415f83708
Saturn Technologies <info@saturn72.com>
2017-12-13 22:06:13 +0200

    add controller base to saturn72

---
+++ src/NetCore/Saturn72.Core.Services/Web/Controllers/Saturn72ControllerBase.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Saturn72.Core.Services.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class Saturn72ControllerBase : Controller
+    {
+    }
+}
