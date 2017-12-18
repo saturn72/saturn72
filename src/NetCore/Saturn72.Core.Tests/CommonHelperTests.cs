6c7ae93407b6af4dcbd1ae3d1b845b1da708708f
Saturn Technologies <info@saturn72.com>
2017-12-18 17:58:51 +0200

    Add CommonHelper

---
+++ src/NetCore/Saturn72.Core.Tests/CommonHelperTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Reflection;
+using Shouldly;
+using Xunit;
+
+namespace Saturn72.Core.Tests
+{
+    public class CommonHelperTests
+
+    {
+        [Fact]
+        public void CommonHelper_GetAssemblyLocalPath()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var expected = new Uri(asm.CodeBase).LocalPath;
+            CommonHelper.GetAssemblyLocalPath(asm).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void CommonHelper_RunTimedoutExpression_TimeoutExceeds()
+        {
+            CommonHelper.RunTimedoutExpression(() => false, 100, 20).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void CommonHelper_RunTimedoutExpression_TrueExpression()
+        {
+            var i = 0;
+            CommonHelper.RunTimedoutExpression(() => i++ == 5, 100, 10).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void CommonHelper_Copy_Throws()
+        {
+            //different type
+            Should.Throw<InvalidOperationException>(
+                () => CommonHelper.Copy(new DummyClass(), new DummyClassChild()));
+        }
+
+        [Fact]
+        public void CommonHelper_Copy_CreateNew()
+        {
+            var source = new DummyClass();
+            var dest = CommonHelper.Copy(source);
+
+            dest.InternalString.ShouldBe(source.InternalString);
+            dest.StringWithSetter.ShouldBe(source.StringWithSetter);
+            dest.StringWithoutSetter.ShouldBe(source.StringWithoutSetter);
+        }
+
+        [Fact]
+        public void CommonHelper_Copy_ToInstance()
+        {
+            var source = new DummyClass
+            {
+                StringWithSetter = "string with setter",
+                InternalString = "internal string"
+            };
+
+            var dest = new DummyClass();
+
+            CommonHelper.Copy(source, dest);
+
+            dest.InternalString.ShouldBe(source.InternalString);
+            dest.StringWithSetter.ShouldBe(source.StringWithSetter);
+            dest.StringWithoutSetter.ShouldBe(source.StringWithoutSetter);
+        }
+
+        [Fact]
+        public void CommonHelper_Copy_ProtectedProperties()
+        {
+            var source = new DummyClassChild
+            {
+                StringWithSetter = "string with setter",
+                InternalString = "internal string"
+            };
+            source.SetProtectedString("value");
+
+            var dest = new DummyClassChild();
+
+            CommonHelper.Copy(source, dest);
+
+            dest.InternalString.ShouldBe(source.InternalString);
+            dest.StringWithSetter.ShouldBe(source.StringWithSetter);
+            dest.StringWithoutSetter.ShouldBe(source.StringWithoutSetter);
+        }
+
+
+        internal class DummyClass
+        {
+            private const string _stringWithoutSetter = "TTT";
+
+            public string StringWithSetter { get; set; }
+
+            public string StringWithoutSetter
+            {
+                get { return _stringWithoutSetter; }
+            }
+
+            internal string InternalString { get; set; }
+            protected string ProtectedString { get; set; }
+        }
+
+        internal class DummyClassChild : DummyClass
+        {
+            public string GetProtectedString
+            {
+                get { return ProtectedString; }
+            }
+
+            public void SetProtectedString(string value)
+            {
+                ProtectedString = value;
+            }
+        }
+    }
+}
---
+++ src/NetCore/Saturn72.Core/CommonHelper.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Saturn72.Core
+{
+    public sealed class CommonHelper
+    {
+        private const string TypeNameFormat = "{0}, {1}";
+
+        public static T Copy<T>(T source) where T : class, new()
+        {
+            var destination = CreateInstance<T>(typeof(T));
+            Copy(source, destination);
+            return destination;
+        }
+
+        public static void Copy<T>(T source, T destination)
+        {
+            if (source.GetType() != destination.GetType())
+                throw new InvalidOperationException("Source and destination must be of the same type");
+
+            var type = typeof(T);
+
+            do
+            {
+                var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (var f in fields)
+                    f.SetValue(destination, f.GetValue(source));
+                type = type.BaseType;
+            } while (type != null);
+        }
+
+        /// <summary>
+        ///     Creates new instance of an object.
+        /// </summary>
+        /// <typeparam name="T">Object type</typeparam>
+        /// <param name="typeName">
+        ///     required object name.
+        ///     <remarks>
+        ///         The name foemat is [full_type_name], [assembly_name]]
+        ///         <example>System, System.String</example>
+        ///     </remarks>
+        /// </param>
+        /// <param name="args">Constructor arguments</param>
+        /// <returns>new instance of </returns>
+        public static T CreateInstance<T>(string typeName, params object[] args) where T : class
+        {
+            var type = GetTypeFromAppDomain(typeName);
+            if (type == null)
+                throw new ArgumentException(typeName);
+
+            return CreateInstance<T>(type, args);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="args">Constructor arguments</param>
+        /// <returns>{T}</returns>
+        public static T CreateInstance<T>(Type type, params object[] args) where T : class
+        {
+            return Activator.CreateInstance(type, args) as T;
+        }
+
+        public static Type TryGetTypeFromAppDomain(string typeName)
+        {
+            var tArrLength = typeName.Split(',').Select(s => s.Trim()).ToArray().Length;
+
+            if (tArrLength == 1)
+            {
+                var allAppDomainTypes = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(x => x.GetTypes());
+                return
+                    allAppDomainTypes.FirstOrDefault(
+                        t => t.Name.Equals(typeName, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            try
+            {
+                return GetTypeFromAppDomain(typeName);
+            }
+            catch
+            {
+            }
+            return null;
+        }
+
+        /// <summary>
+        ///     Fetches type by scanning app domain
+        /// </summary>
+        /// <param name="typeName">
+        ///     required object name.
+        ///     <remarks>
+        ///         The name foemat is [full_type_name], [assembly_name]]
+        ///         <example>System, System.String</example>
+        ///     </remarks>
+        /// </param>
+        /// <returns>new instance of </returns>
+        public static Type GetTypeFromAppDomain(string typeName)
+        {
+            var result = Type.GetType(typeName);
+            if (result != null)
+                return result;
+
+            var split = typeName.Split(',').Select(s => s.Trim()).ToArray();
+            if (split.Length != 2)
+                throw new ArgumentException("typeName");
+            return GetTypeFromAppDomain(split[0], split[1]);
+        }
+
+        public static Type GetTypeFromAppDomain(string typeFullName, string assemblyName)
+        {
+            var asm = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .FirstOrDefault(a => a.GetName().Name == assemblyName);
+
+            if (asm == null)
+                throw new ArgumentException(string.Format("The assembly {0} was not found in app domain", assemblyName),
+                    assemblyName);
+
+            return asm.GetType(typeFullName);
+        }
+
+
+        public static string GetCompatibleTypeName<T>()
+        {
+            return GetCompatibleTypeName(typeof(T));
+        }
+
+        public static string GetCompatibleTypeName(Type type)
+        {
+            return string.Format(TypeNameFormat, type.FullName, type.Assembly.GetName().Name);
+        }
+
+        /// <summary>
+        ///     Runs expression with timeout flag
+        /// </summary>
+        /// <param name="expression">Expression to examined</param>
+        public static bool RunTimedoutExpression(Func<bool> expression)
+        {
+            return RunTimedoutExpression(expression, 2000);
+        }
+
+        /// <summary>
+        ///     Runs expression with timeout flag
+        /// </summary>
+        /// <param name="expression">Expression to examined</param>
+        /// <param name="totalMiliSecTimeout">Total time out in milisecs</param>
+        public static bool RunTimedoutExpression(Func<bool> expression, uint totalMiliSecTimeout)
+        {
+            return RunTimedoutExpression(expression, totalMiliSecTimeout, 50);
+        }
+
+        /// <summary>
+        ///     Runs expression with timeout flag
+        /// </summary>
+        /// <param name="expression">Expression to examined</param>
+        /// <param name="totalMiliSecTimeout">Total time out</param>
+        /// <param name="milisecInterval">Sleep interval between expression execution</param>
+        public static bool RunTimedoutExpression(Func<bool> expression, uint totalMiliSecTimeout, uint milisecInterval)
+        {
+            bool result;
+            var startTime = DateTime.Now;
+
+            while (!(result = expression()))
+            {
+                Thread.Sleep((int) milisecInterval);
+
+                if (DateTime.Now.Subtract(startTime).TotalMilliseconds > totalMiliSecTimeout)
+                    return false;
+            }
+
+            return result;
+        }
+
+        public static string GetAssemblyLocalPath(Assembly asm)
+        {
+            var uri = new Uri(asm.CodeBase);
+            return uri.LocalPath;
+        }
+    }
+}
